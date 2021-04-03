ccefd4f534db19418050a9c243f44c118c36e083
swade781977 <swade781977@yahoo.com>
2021-04-03 01:17:52 -0400

    commit tree 4bbe5463063470b7130ea8a02e4a6d1634f66973

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,29 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor(){
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL)
+            .then(resp => resp.json())
+            .then(data => this.setState({reviews: data.results}))
+    }
+
+    render(){
+        return(
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+
+
+}
+
+export default LatestMovieReviewsContainer
